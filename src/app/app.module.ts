4ea4ce5e57be86d10e22c0bd1d764ca7e3c9fb24
jbcoliveira <jbcoliveira1984@gmail.com>
2022-06-12 22:08:50 -0300

    Definindo componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './nav/menu/menu.component';
+import { HomeComponent } from './nav/home/home.component';
+import { FooterComponent } from './nav/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
