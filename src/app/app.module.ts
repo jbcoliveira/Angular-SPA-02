14372c7152ed4fabfcb228b102c390c5fc7208ca
jbcoliveira <jbcoliveira1984@gmail.com>
2022-06-12 22:33:13 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './nav/menu/menu.component';
 import { HomeComponent } from './nav/home/home.component';
 import { FooterComponent } from './nav/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash:false})]
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/'}    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
